dbd7453f1e277a50894f04ec769e339b5c03d2e4
mohammed jafran <96252493+MJ512@users.noreply.github.com>
2026-03-25 12:18:27 +0530

    Delete src/main directory

--- src/main/java/org/bookingsystemapi/Interface/BookingDAOInterface.java
+++
@@ -1,8 +0,0 @@
-package org.bookingsystemapi.Interface;
-
-import org.bookingsystemapi.model.Booking;
-
-public interface BookingDAOInterface {
-    int createBooking(Booking booking);
-    boolean cancelBooking(int bookingId);
-}
